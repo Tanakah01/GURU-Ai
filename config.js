10bd1ba8fa211a2235b7a88c67c1fc3868207e30
Tanakah01 <168909730+Tanakah01@users.noreply.github.com>
2024-08-18 02:47:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const ownervb = process.env.OWNERS || "917605902011;GURU;6281255369012;Ash"
+const ownervb = process.env.OWNERS || "263776404156;GURU;263776404156;Ash"
 
 
 const ownerlist = ownervb.split(';');
